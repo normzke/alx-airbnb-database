a5affdec0749af0f3b926af102123974d26716ea
normzke <65168275+normzke@users.noreply.github.com>
2025-05-18 09:19:16 +0000

    Fix: Added ORDER BY to LEFT JOIN query

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -6,7 +6,9 @@ INNER JOIN users u ON b.user_id = u.id;
 -- 2. LEFT JOIN: Retrieve all properties and their reviews (including properties that have no reviews)
 SELECT p.id AS property_id, p.name, r.id AS review_id, r.rating, r.comment
 FROM properties p
-LEFT JOIN reviews r ON p.id = r.property_id;
+LEFT JOIN reviews r ON p.id = r.property_id
+ORDER BY p.id;
+
 
 -- 3. FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or booking is not linked to a user
 SELECT u.id AS user_id, u.name, b.id AS booking_id, b.property_id, b.start_date
