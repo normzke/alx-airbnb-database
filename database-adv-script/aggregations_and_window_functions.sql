369ba7e1d8904631fef8e0d937044c40a9a8fed7
normzke <65168275+normzke@users.noreply.github.com>
2025-05-18 09:31:19 +0000

    Updated to use ROW_NUMBER() for property ranking

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -2,9 +2,11 @@ SELECT user_id, COUNT(*) AS total_bookings
 FROM bookings
 GROUP BY user_id;
 
+
 SELECT 
     property_id,
     COUNT(*) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
 FROM bookings
 GROUP BY property_id;
+
